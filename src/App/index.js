816acbe7669828417a660bff992762dd5527165d
tomasotano25 <tomasotano25@gmail.com>
2022-09-21 14:46:23 -0400

    Organizar archivos 1

--- src/App/index.js
+++ src/App/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 // import "./App.css";
-import { TodoCounter } from "./components/TodoCounter";
-import { TodoSearch } from "./components/TodoSearch";
-import { TodoList } from "./components/TodoList";
-import { TodoItem } from "./components/TodoItem";
-import { CreateTodoButton } from "./components/CreateTodoButton";
+import { TodoCounter } from "../components/TodoCounter";
+import { TodoSearch } from "../components/TodoSearch";
+import { TodoList } from "../components/TodoList";
+import { TodoItem } from "../components/TodoItem";
+import { CreateTodoButton } from "../components/CreateTodoButton";
 
 const defaultTodos = [
   { text: "Cortar cebolla", completed: true },
